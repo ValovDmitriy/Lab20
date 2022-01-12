28dc7d6ec4bdc14b75f471e6f1da6c6570100af3
Дмитрий Валов <dmitr.valov@yandex.ru>
2022-01-12 22:04:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            DSV dsv = Сircumference;
+            dsv += AreaCircle;
+            dsv += VolumeSphere;
+            dsv(rad);
+        }
+        static double Сircumference(double radius)
+        {
+            double d = 2 * Math.PI * radius;
+            Console.WriteLine("Длина окружности - {0}", d);
+            return d;
+        }
+        static double AreaCircle(double radius)
+        {
+            double s = Math.PI * radius * radius;
+            Console.WriteLine("Площадь окружности - {0}", s);
+            return s;
+        }
+
+        static double VolumeSphere(double radius)
+        {
+            double v = 4 / 3 * Math.PI * radius * radius * radius;
+            Console.WriteLine("Объем сферы - {0}", v);
+            return v;
+        }
+        delegate double DSV(double radius);
+    }
+}
